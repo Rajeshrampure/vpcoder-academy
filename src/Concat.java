bcef4118881ff35061df408467b60a6e4aac68cf
Rajeshrampure <rajeshrampure1997@gmail.com>
2022-05-31 20:26:10 +0530

    Adding cal & concat files

---
+++ src/Concat.java
@@ -0,0 +1,21 @@
+public class Concat {
+    public static void main(String[] args) {
+        String name="Rajesh";
+        short age=25;
+        int salary=45000;
+        float bankBalance=24500000;
+        char currency='$';
+        long mobileNumber=968693317;
+        double licMaturity=58000000.88855555884888;
+        boolean declareAboveStatements=true;
+
+        System.out.print(name+" is "+age+" with Annual salary "+salary+currency+" and bank balance is "+bankBalance+currency
+        +" mobile number is "+mobileNumber+" has LIC policy with maturity amount "+licMaturity+currency);
+
+    }
+//    public static String data()
+//    {
+//        return data;
+//    }
+
+}
