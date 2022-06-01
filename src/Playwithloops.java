35ec2e337c92b75f40951c1889fc7ac058ef5d2f
Rajeshrampure <rajeshrampure1997@gmail.com>
2022-06-01 20:19:55 +0530

    Add name and reverse number

---
+++ src/Playwithloops.java
@@ -0,0 +1,9 @@
+public class Playwithloops {
+    public static void main(String[] args) {
+        Test1 t1 = new Test1();
+        t1.name();
+        t1.number();
+        t1.additions();
+    }
+}
+
---
+++ src/Test1.java
@@ -0,0 +1,25 @@
+public class Test1 {
+
+    public void name(){
+        int k=1;
+        for(int i=0;i<2;i++){
+            for(int j=0;j<5;j++){
+                System.out.println("My name is Rajesh "+k);
+                k++;
+            }
+        }
+    }
+    public void number(){
+        for(int a=100;a>0;a--){
+            System.out.println(a);
+        }
+
+    }
+    public void additions(){
+        for(int b=0;b<10;b=b+1){
+        int c=1;
+        int
+            System.out.println(b);
+    }
+
+}
